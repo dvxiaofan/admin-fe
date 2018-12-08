75f7a5b49a558a200cf663062f6a7084b61c75ac
xiaofan <1353815633@qq.com>
2018-12-08 20:14:47 +0800

    webpack config02

--- src/app.jsx
+++ src/app.jsx
@@ -2,15 +2,19 @@
  * @Author: xiaofan 
  * @Date: 2018-12-07 23:49:58 
  * @Last Modified by: xiaofan
- * @Last Modified time: 2018-12-08 00:27:29
+ * @Last Modified time: 2018-12-08 20:11:09
  */
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import 'font-awesome/css/font-awesome.min.css';
 import './index.css';
+import './index.scss';
 
 ReactDOM.render( 
-	<h1> Hello, world! </h1>,
+	<div>
+		<h1> Hello, Xiaofan! </h1>
+	</div>,
 	document.getElementById('app')
 );
--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,25 @@
  * @Author: xiaofan 
  * @Date: 2018-12-07 23:49:29 
  * @Last Modified by: xiaofan
- * @Last Modified time: 2018-12-08 00:22:27
+ * @Last Modified time: 2018-12-08 20:12:57
  */
 
-const path = require('path');
+const path 							= require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack 					= require('webpack');
 
 module.exports = {
 	entry: './src/app.jsx',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'app.jsx'
+		publicPath:	'/dist/',
+		filename: 'js/app.js'
 	},
 	module: {
-		rules: [{
+		rules: [
+			// React(jsx) 处理
+			{
 				test: /\.m?jsx$/,
 				exclude: /(node_modules)/,
 				use: {
@@ -25,16 +30,66 @@ module.exports = {
 					}
 				}
 			},
+			// css 处理
 			{
 				test: /\.css$/,
-				use: ['style-loader', 'css-loader'],
+				use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        }),
+			},
+			// sass 处理
+			{
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+			},
+			// 图片 处理
+			{
+        test: /\.(png|jpg|gif|)$/,
+        use: [
+          {
+            loader	: 'url-loader',
+            options	: {
+							limit	: 8192,
+							name	: 'resource/[name].[ext]'
+            }
+          }
+        ]
+			},
+			// 字体 处理
+			{
+				test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
+				use: [
+					{
+						loader	: 'file-loader',
+						options	: {
+							limit	: 8192,
+							name	:	'resource/[name].[ext]'
+						}
+					}
+				]
 			},
 		]
 	},
 	plugins: [
+		// 处理html
 		new HtmlWebpackPlugin({
 			template: './src/index.html'
+		}),
+		// 处理CSS
+		new ExtractTextPlugin("css/[name].css"),
+		// 提出公共模块
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'common',
+			filename: 'js/base.js'
 		})
 	],
+	devServer: {
+		port: 8086,
+    inline: true,
+	},
 
 };

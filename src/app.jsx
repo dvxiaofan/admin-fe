81dd859cf1cdf2fc2bfa6525bdd319971ee5b7cf
xiaofan <1353815633@qq.com>
2018-12-10 23:38:29 +0800

    init react

--- src/app.jsx
+++ src/app.jsx
@@ -2,7 +2,7 @@
  * @Author: xiaofan 
  * @Date: 2018-12-07 23:49:58 
  * @Last Modified by: xiaofan
- * @Last Modified time: 2018-12-09 23:53:08
+ * @Last Modified time: 2018-12-10 23:38:21
  */
 
 import React from 'react';
@@ -10,28 +10,3 @@ import ReactDOM from 'react-dom';
 
 import './index.scss';
 
-// let style = {
-// 	color: 'blue',
-// 	fontSize: '38px'
-// }
-let flag = true;
-let name1 = 'name';
-let name2 = 'name2';
-
-let names = ['name1', 'name2', 'name3', 'name4'];
-
-let jsx = (
-	<div className="jsx">
-		{/* 条件判断 */}
-		{
-			flag ? <p>Hello {name1}</p> : <p>Hello {name2}</p>
-		}
-		{/* 循环 */}
-		{
-			names.map((name, index) => <p key={index}>Hello {name}</p>)
-		}
-	</div>
-);
-
-
-ReactDOM.render(jsx, document.getElementById('app'));

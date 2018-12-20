d3e69e405496a2734aecab809f126b89fc00a3aa
xiaofan <1353815633@qq.com>
2018-12-20 21:41:55 +0800

    头部菜单

--- src/component/top-nav/index.jsx
+++ src/component/top-nav/index.jsx
@@ -2,40 +2,50 @@
  * @Author: xiaofan 
  * @Date: 2018-12-18 21:12:58 
  * @Last Modified by: xiaofan
- * @Last Modified time: 2018-12-18 21:59:53
+ * @Last Modified time: 2018-12-20 21:41:42
  */
 
 
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 class TopNav extends React.Component {
 	render() {
 		return (
-			<nav className="navbar navbar-default top-navbar" role="navigation">
+			<nav className="navbar navbar-default top-navbar">
 				<div className="navbar-header">
-					<a className="navbar-brand" href="index.html"><b>X</b>Mall</a>
+					<Link className="navbar-brand" to="/"><b>HELLO</b>XMALL</Link>
 				</div>
 
 				<ul className="nav navbar-top-links navbar-right">
 					<li className="dropdown">
-						
-						<a className="dropdown-toggle" data-toggle="dropdown" href="#" aria-expanded="false">
-							<i className="fa fa-user fa-fw"></i> <i className="fa fa-caret-down"></i>
+						<a className="dropdown-toggle" href="javascript:;">
+							<i className="fa fa-user fa-fw"></i>
+							<span>欢迎，admin xxx</span>
+							<i className="fa fa-caret-down"></i>
 						</a>
 
 						<ul className="dropdown-menu dropdown-user">
-							
-							<li><a href="#"><i className="fa fa-sign-out fa-fw"></i> 退出登录</a>
+							<li>
+								<a onClick={ () => { this.onLogout() } }>
+									<i className="fa fa-sign-out fa-fw"></i>
+									<span>退出登录</span>
+								</a>
 							</li>
 						</ul>
-
 					</li>
 				</ul>
 			</nav>
 		)
 	}
+
+	// 退出登录
+	onLogout() {
+		console.log('onLogout');
+	}
+
 }
 
 

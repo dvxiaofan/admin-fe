68455cf5cbaa1c5bd970055d308851947482560b
xiaofan <1353815633@qq.com>
2019-05-11 15:43:57 +0800

    完成导航栏和侧边栏开发

--- src/component/nav-top/index.jsx
+++ src/component/nav-top/index.jsx
@@ -2,11 +2,12 @@
  * @Author: DevZhang 
  * @Date: 2019-05-10 14:09:45 
  * @Last Modified by: DevZhang
- * @Last Modified time: 2019-05-11 14:46:46
+ * @Last Modified time: 2019-05-11 15:20:41
  */
 
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 class NavTop extends React.Component {
@@ -16,21 +17,22 @@ class NavTop extends React.Component {
     
     render() {
         return (
-			<nav className="navbar navbar-default top-navbar" role="navigation">
+			<nav className="navbar navbar-default top-navbar">
 				<div className="navbar-header">
-					<a className="navbar-brand" href="index.html"><b>X</b>Mall</a>
+					<Link to="/" className="navbar-brand"><b>X</b>MALL</Link>
 				</div>
 
 				<ul className="nav navbar-top-links navbar-right">
 					<li className="dropdown">
 						
-						<a className="dropdown-toggle" data-toggle="dropdown" href="#" aria-expanded="false">
-							<i className="fa fa-user fa-fw"></i> <i className="fa fa-caret-down"></i>
+						<a className="dropdown-toggle" href="javascript:;">
+							<i className="fa fa-user fa-fw"></i>
+							<span>欢迎，admin用户</span>
+							<i className="fa fa-caret-down"></i>
 						</a>
 
 						<ul className="dropdown-menu dropdown-user">
-							
-							<li><a href="#"><i className="fa fa-sign-out fa-fw"></i> 退出登录</a>
+							<li><a><i className="fa fa-sign-out fa-fw"></i> 退出登录</a>
 							</li>
 						</ul>
 
